7bc84eb8493a88bfe15616aaa3a9eb4ff5b750f5
Lucas Silva <lucasimap@alu.ufc.br>
2020-04-11 14:57:37 -0300

    Adicionando ENV

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseUrl: 'http://localhost:3333'
+  baseUrl: process.env.REACT_APP_API_URL,
 });
-
 
 export default api;
